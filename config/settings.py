95b9e3cc27fe524a19f83452cb14b46ea8f76987
Farhat <f190886@mail.ru>
2025-10-28 21:38:44 +0400

    Добавил в проект рекомендации, так же проверил проект с помощью библиотек

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ class Settings:
         if not base_url:
             raise ValueError("Ошибка требуется BASE_URL")
         if not valid_token:
-            raise ValueError("Ошибка требуеься VALID_TOKEN")
+            raise ValueError("Ошибка требуется VALID_TOKEN")
 
         self.BASE_URL = base_url.rstrip("/")
         self.VALID_TOKEN = valid_token
